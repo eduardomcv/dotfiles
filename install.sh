545086192ab62e95c6ae0fdd94572246d9f1c425
Eduardo Vicente <eduardomcv42@gmail.com>
2020-04-30 00:15:42 +0100

    added dotfiles and tools functions. added main with conditions for arguments

--- install.sh
+++ install.sh
@@ -1,9 +1,30 @@
 set -e
 set -o pipefail
 
-rsync --quiet \
-    --exclude ".git/" \
-    --exclude "install" \
-    --exclude "README.md" \
-     -avh --no-perms . ~
+dotfiles() {
+  rsync --quiet \
+      --exclude ".git/" \
+      --exclude "install" \
+      --exclude "README.md" \
+      --exclude ".bashrc" \
+      -avh --no-perms . ~
 
+  cat .bashrc >> ~/.bashrc
+}
+
+tools() {
+  # Node Version Manager
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+}
+
+main() {
+  local cmd=$1
+
+  if [[ $cmd == "dotfiles" ]]; then
+    dotfiles
+  elif [[ $cmd == "tools" ]]; then
+    tools
+  fi
+}
+
+main "$@"
