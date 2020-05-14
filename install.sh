b6a6ba0e7e4b93751843279e69fe149c200542a1
Eduardo Vicente <eduardomcv42@gmail.com>
2020-05-14 17:06:19 +0100

    added docker-compose

--- install.sh
+++ install.sh
@@ -59,8 +59,13 @@ tools() {
   # Node Version Manager
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+  # docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+
   # setup docker for non-root
   usermod -aG docker $USER
+  systemctl enable docker
 }
 
 main() {
