ef2c9ac1f166d517696016d8101c8a81555ab5e9
Eduardo Vicente <eduardomcv42@gmail.com>
2020-05-14 21:22:55 +0100

    rm microsoft trash

--- install.sh
+++ install.sh
@@ -77,6 +77,7 @@ setup_system() {
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
   install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
   sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm packages.microsoft.gpg
 
   ## spotify
   curl -sS https://download.spotify.com/debian/pubkey.gpg | apt-key add - 
