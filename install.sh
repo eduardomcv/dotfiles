6af317be9969836da492b7033575ffdf7f6dc8c5
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-15 22:01:54 +0100

    added vim

--- install.sh
+++ install.sh
@@ -101,7 +101,8 @@ setup_system() {
     google-chrome-stable \
     spotify-client \
     slack-desktop \
-    gimp
+    gimp \
+    vim
 
   # docker-compose
   curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
