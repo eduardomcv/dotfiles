2224a46cc330eeabfbb70b4e12fd569a3ef76a51
Eduardo Vicente <eduardomcv42@gmail.com>
2020-08-14 18:29:59 +0100

    added vscode repository as per vscode docs

--- install.sh
+++ install.sh
@@ -72,8 +72,8 @@ setup_system() {
 
   ## vscode
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-  install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-  sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
   rm packages.microsoft.gpg
 
   ## chrome
