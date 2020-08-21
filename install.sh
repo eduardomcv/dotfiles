fffe9d8898b74c287f797f4ac5b5bf6f62230730
Eduardo Vicente <eduardomcv42@gmail.com>
2020-08-21 15:27:07 +0100

    added vim-gtk

--- install.sh
+++ install.sh
@@ -104,7 +104,7 @@ setup_system() {
     spotify-client \
     slack-desktop \
     gimp \
-    vim
+    vim-gtk
 
   # docker-compose
   curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
