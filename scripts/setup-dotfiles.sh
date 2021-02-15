41c0bcc9c6d73360dd4a5fe9d4d7c859b0a89260
Eduardo Vicente <eduardomcv42@gmail.com>
2021-02-15 02:25:16 +0000

    redo rsync

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -6,16 +6,5 @@ source ./utils/sudo.sh
 
 check_is_not_sudo
 
-rsync --quiet \
-  --exclude ".git/" \
-  --exclude "utils/" \
-  --exclude "install.sh" \
-  --exclude "README.md" \
-  --exclude ".bashrc" \
-  --exclude "git-prompt.txt" \
-  --exclude "logid.cfg" \
-  -avh --no-perms . ~
+rsync --quiet -avh --no-perms ../sync ~
 
-# WIP
-# setup git bash prompt
-cat git-prompt.txt >> ~/.bashrc
