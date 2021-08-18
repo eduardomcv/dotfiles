a618f110e16fa51e19917d7d2a1e082e85eb9ecc
Eduardo Vicente <eduardomcv42@gmail.com>
2021-08-19 00:01:09 +0100

    add dirs

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -6,7 +6,19 @@ source ./utils/sudo.sh
 
 check_is_not_sudo
 
-echo Synching...
-stow -t ~ 
+THINGS_TO_STOW=( \
+    fish \
+    git \
+    neovim \
+    tmux \
+    vscode \
+    wallpapers \
+)
+
+for thing in ${THINGS_TO_STOW[@]}; do
+    stow -vDt ~ $thing
+    stow -vt ~ $thing
+done
+
 echo Done.
 
