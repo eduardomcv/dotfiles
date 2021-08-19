74637287be2f748679891de3b182c4b634b46c0e
Eduardo Vicente <eduardomcv42@gmail.com>
2021-08-19 21:51:49 +0100

    add arch stuff. some renames.

---
+++ system-install/install-arch-base.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source ./utils/sudo.sh
+
+check_is_sudo
+
+echo Installing packages...
+pacman -Syu --needed --noconfirm \
+  base-devel \
+  git \
+  docker \
+  docker-compose \
+  ttf-fira-code \
+  ctags \
+  gimp \
+  neovim \
+  tmux \
+  fish \
+  stow \
+  discord
+
