437517e3616fdf062a0d829d183e447aff0d0fff
Eduardo Vicente <eduardomcv42@gmail.com>
2021-11-27 18:54:31 +0000

    add completion item kind

--- neovim/.config/nvim/plugin/nvim-lspconfig.rc.lua
+++ neovim/.config/nvim/plugin/nvim-lspconfig.rc.lua
@@ -39,6 +39,34 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
     vim.api.nvim_command [[augroup END]]
   end
+
+  protocol.CompletionItemKind = {
+    '', -- Text
+    '', -- Method
+    '', -- Function
+    '', -- Constructor
+    '', -- Field
+    '', -- Variable
+    '', -- Class
+    'ﰮ', -- Interface
+    '', -- Module
+    '', -- Property
+    '', -- Unit
+    '', -- Value
+    '', -- Enum
+    '', -- Keyword
+    '﬌', -- Snippet
+    '', -- Color
+    '', -- File
+    '', -- Reference
+    '', -- Folder
+    '', -- EnumMember
+    '', -- Constant
+    '', -- Struct
+    '', -- Event
+    'ﬦ', -- Operator
+    '', -- TypeParameter
+  }
 end
 
 -- Set up completion using nvim_cmp with LSP source
