c7417574aef9cd4fd6faae36d43a26c361dbe771
Eduardo Vicente <eduardomcv42@gmail.com>
2021-11-28 19:20:00 +0000

    add diagnostics language server. comment some maps

--- neovim/.config/nvim/plugin/lspconfig.rc.lua
+++ neovim/.config/nvim/plugin/lspconfig.rc.lua
@@ -1,4 +1,4 @@
-local nvim_lsp = require('lspconfig')
+local lspconfig = require('lspconfig')
 local protocol = require('vim.lsp.protocol')
 local cmp = require('cmp_nvim_lsp')
 
@@ -12,14 +12,17 @@ local on_attach = function(client, bufnr)
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = {
+    noremap=true,
+    silent=true,
+  }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   -- buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -28,10 +31,15 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  -- buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+  -- let diagnosticls handle the formatting instead of tsserver
+  if client.name == 'tsserver' then
+    client.resolved_capabilities.document_formatting = false
+  end
 
   if client.resolved_capabilities.document_formatting then
     vim.api.nvim_command [[augroup Format]]
@@ -74,7 +82,8 @@ local capabilities = cmp.update_capabilities(
   protocol.make_client_capabilities()
 )
 
-nvim_lsp.tsserver.setup{
+-- Typescript language server
+lspconfig.tsserver.setup{
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {
@@ -86,3 +95,98 @@ nvim_lsp.tsserver.setup{
     "typescript.tsx"
   },
 }
+
+-- Diagnostics language server
+lspconfig.diagnosticls.setup{
+  on_attach = on_attach,
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+  init_options = {
+    linters = {
+      eslint = {
+        command = 'eslint_d',
+        rootPatterns = {
+          'package.json',
+          '.git',
+        },
+        debounce = 100,
+        args = {
+          '--stdin',
+          '--stdin-filename',
+          '%filepath',
+          '--format',
+          'json'
+        },
+        sourceName = 'eslint_d',
+        parseJson = {
+          errorsRoot = '[0].messages',
+          line = 'line',
+          column = 'column',
+          endLine = 'endLine',
+          endColumn = 'endColumn',
+          message = '[eslint] ${message} [${ruleId}]',
+          security = 'severity'
+        },
+        securities = {
+          [2] = 'error',
+          [1] = 'warning'
+        }
+      },
+    },
+    filetypes = {
+      javascript = 'eslint',
+      javascriptreact = 'eslint',
+      typescript = 'eslint',
+      typescriptreact = 'eslint',
+    },
+    formatters = {
+      eslint = {
+        command = 'eslint_d',
+        rootPatterns = {
+          'package.json',
+          '.git',
+        },
+        args = {
+          '--stdin',
+          '--fix-to-stdout',
+          '--stdin-filename',
+          '%filename',
+        },
+      },
+      prettier = {
+        command = 'prettier_d_slim',
+        rootPatterns = {
+          'package.json',
+          '.git',
+        },
+        requiredFiles = { 'prettier.config.js' },
+        args = {
+          '--stdin',
+          '--stdin-filepath',
+          '%filename'
+        }
+      }
+    },
+    formatFiletypes = {
+      javascript = 'eslint',
+      javascriptreact = 'eslint',
+      typescript = 'eslint',
+      typescriptreact = 'eslint',
+    }
+  }
+}
+
+-- icon for diagnistics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    virtual_text = {
+      spacing = 4,
+      prefix = ''
+    }
+  }
+)
