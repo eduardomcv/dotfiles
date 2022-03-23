6d89884784903b68720595234191ab09c9649c86
Eduardo Vicente <eduardomcv42@gmail.com>
2022-03-23 23:34:58 +0000

    fix issues

--- neovim/.config/nvim/plugin/treesitter.lua
+++ neovim/.config/nvim/plugin/treesitter.lua
@@ -20,5 +20,5 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.used_by = { "typescript.tsx" }
+local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+ft_to_parser.tsx = "typescript.tsx"
