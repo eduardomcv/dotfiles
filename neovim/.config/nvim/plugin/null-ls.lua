366094323d785d1b4a608d31a9852c2ccce27ae2
Eduardo Vicente <eduardomcv42@gmail.com>
2022-10-22 19:46:50 +0100

    Add conditional formatting for prettierd

--- neovim/.config/nvim/plugin/null-ls.lua
+++ neovim/.config/nvim/plugin/null-ls.lua
@@ -14,7 +14,11 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettierd.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.prettierrc', '.prettierrc.json', '.prettierrc.yml', '.prettierrc.js' })
+      end
+    }),
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
