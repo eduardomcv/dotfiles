c49151d8e556536157ee9480b64c77cb3297dfe5
Eduardo Vicente <eduardomcv42@gmail.com>
2022-10-23 15:58:34 +0100

    Retire wallpapers. Change install script to .sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -euo pipefail
+
+source ./utils/sudo.sh
+
+check_is_not_sudo
+
+THINGS_TO_STOW=( \
+    git \
+    zsh \
+    neovim \
+    tmux \
+    vscode \
+    kitty \
+)
+
+for thing in ${THINGS_TO_STOW[@]}; do
+    stow -vDt ~ $thing
+    stow -vt ~ $thing
+done
+
+echo Done.
+
