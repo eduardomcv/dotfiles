965a8d91f933837ead7b4c5f1b7ed681e312ea33
Eduardo Vicente <eduardomcv42@gmail.com>
2022-10-23 22:23:42 +0100

    remove eol character

--- neovim/.config/nvim/after/plugin/indent-blankline.lua
+++ neovim/.config/nvim/after/plugin/indent-blankline.lua
@@ -2,10 +2,9 @@ local status, indent_blankline = pcall(require, 'indent_blankline')
 
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append "eol:↴"
 
 indent_blankline.setup {
   space_char_blankline = " ",
-  show_end_of_line = true,
+  show_end_of_line = false,
   show_current_context = true,
 }
