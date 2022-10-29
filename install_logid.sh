24097040bc6e0ff2ac0538a3d71726e835af2270
Eduardo Vicente <eduardomcv42@gmail.com>
2022-10-29 19:23:23 +0100

    Move logid config and install script

--- install_logid.sh
+++ install_logid.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-source ./sudo.sh
+source ./utils/sudo.sh
 
 check_is_sudo
 
 echo Setting up logitech mouse config...
 
-cp ../config/logid.cfg /etc/logid.cfg
+cp ./logid/logid.cfg /etc/logid.cfg
 
 echo Done.
