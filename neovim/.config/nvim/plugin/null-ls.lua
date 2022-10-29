4a6f918c65a23add4e6ea382da817c1a82539a27
Eduardo Vicente <eduardomcv42@gmail.com>
2022-10-29 19:18:15 +0100

    Remove condition for prettierd. Add code actions and formatting from eslint_d.

--- neovim/.config/nvim/plugin/null-ls.lua
+++ neovim/.config/nvim/plugin/null-ls.lua
@@ -14,14 +14,12 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd.with({
-      condition = function(utils)
-        return utils.root_has_file({ '.prettierrc', '.prettierrc.json', '.prettierrc.yml', '.prettierrc.js' })
-      end
-    }),
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.prettierd,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
