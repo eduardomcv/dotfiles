9b3e1290e3c88f527415c7d8952ce5201777310e
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-11 20:31:02 +0000

    Add aerial to lualine

--- neovim/.config/nvim/after/plugin/lualine.lua
+++ neovim/.config/nvim/after/plugin/lualine.lua
@@ -22,11 +22,14 @@ lualine.setup {
       'branch',
       'diff',
     },
-    lualine_c = { {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
-    } },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      },
+      'aerial',
+    },
     lualine_x = {
       {
         'diagnostics',
