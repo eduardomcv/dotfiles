ca6902352d00384fdd0e0bbec99ce90b62ba9ef1
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-16 16:19:57 +0000

    Comment hover_doc

--- neovim/.config/nvim/plugin/lspsaga.lua
+++ neovim/.config/nvim/plugin/lspsaga.lua
@@ -1,17 +1,13 @@
 local status, lspsaga = pcall(require, "lspsaga")
 if (not status) then return end
 
-lspsaga.init_lsp_saga {
-  server_filetype_map = {
-    typescript = 'typescript'
-  }
-}
+lspsaga.init_lsp_saga()
 
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+-- vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
