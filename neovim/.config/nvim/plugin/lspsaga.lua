d19ede821fe0c73522d9f0ad5e433cd09b998dd2
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-18 23:57:29 +0000

    Refactor

--- neovim/.config/nvim/plugin/lspsaga.lua
+++ neovim/.config/nvim/plugin/lspsaga.lua
@@ -3,12 +3,13 @@ if (not status) then return end
 
 lspsaga.init_lsp_saga()
 
+local set = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
--- vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
-vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
-vim.keymap.set('n', '<leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
+set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+-- set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
+set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
+set('n', '<leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
