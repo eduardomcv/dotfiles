6b6d96641ddebbc063a4f2b53341ebc9fcc8ef2e
Eduardo Vicente <eduardomcv42@gmail.com>
2022-11-24 00:11:24 +0000

    Enable hover_doc

--- neovim/.config/nvim/plugin/lspsaga.lua
+++ neovim/.config/nvim/plugin/lspsaga.lua
@@ -8,7 +8,7 @@ local opts = { noremap = true, silent = true }
 
 set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
--- set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
