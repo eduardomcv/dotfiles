0947f0031925630021eb7d25b319f509d1596834
Eduardo Vicente <eduardo.vicente@blueprism.com>
2022-12-15 09:10:20 +0000

    Add wsl support

--- neovim/.config/nvim/lua/eduardomcv/clipboard.lua
+++ neovim/.config/nvim/lua/eduardomcv/clipboard.lua
@@ -4,6 +4,21 @@ if has "unix" then
   vim.opt.clipboard:append { 'unnamedplus' }
 end
 
+if has "wsl" then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i",
+      ["*"] = "win32yank.exe -i",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o",
+      ["*"] = "win32yank.exe -o",
+    },
+    cache_enabled = 0,
+  }
+end
+
 if has "win32" then
   vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
 end
