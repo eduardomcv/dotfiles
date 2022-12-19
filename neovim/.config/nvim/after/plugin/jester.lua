bc9f2513bdca5aa21be9b78b37a6d7fbd6577a56
Eduardo Vicente <eduardo.vicente@blueprism.com>
2022-12-19 18:19:41 +0000

    Change jest run path

--- neovim/.config/nvim/after/plugin/jester.lua
+++ neovim/.config/nvim/after/plugin/jester.lua
@@ -1,4 +1,9 @@
-local jester = require('jester')
+local ok, jester = pcall(require, 'jester')
+if not ok then return end
+
+jester.setup {
+  path_to_jest_run = './node_modules/.bin/jest',
+}
 
 vim.keymap.set('n', '<leader>tr', jester.run, {})
 vim.keymap.set('n', '<leader>tf', jester.run_file, {})
