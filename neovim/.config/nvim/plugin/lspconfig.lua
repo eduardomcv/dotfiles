299b891ec7bec3cbb3811094390e29bb32d0aa97
Eduardo Vicente <eduardomcv42@gmail.com>
2022-12-21 20:39:58 +0000

    Refactor lspconfig using mason-lspconfig

--- neovim/.config/nvim/plugin/lspconfig.lua
+++ neovim/.config/nvim/plugin/lspconfig.lua
@@ -1,31 +1,6 @@
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then return end
 
--- Mason must be loaded before lspconfig
-require('mason').setup {}
-require('mason-lspconfig').setup {
-  automatic_installation = true
-}
-
--- neodev must be loaded before lspconfig
-require('neodev').setup()
-
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
-
-local enable_format_on_save = function(_, bufnr)
-  vim.api.nvim_clear_autocmds({
-    group = augroup_format,
-    buffer = bufnr
-  })
-
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = augroup_format,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ bufnr = bufnr })
-    end,
-  })
-end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -54,7 +29,8 @@ local on_attach = function(client, bufnr)
 end
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Completion icons
 vim.lsp.protocol.CompletionItemKind = {
@@ -125,63 +101,91 @@ vim.diagnostic.config({
 
 -- Server configurations
 local servers = {
-  'cssls',
-  'html',
-  'jsonls',
-  'emmet_ls',
-  'clangd',
-  'rust_analyzer',
-}
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-  }
-end
-
-require('typescript').setup {
-  server = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    init_options = {
-      plugins = {
-        {
-          name = 'typescript-styled-plugin',
-          location = '/Users/eduardo/.nvm/versions/node/v16.14.2/lib' -- Path to global node modules install location
-        },
-      },
-    },
-  }
-}
-
-lspconfig.sumneko_lua.setup {
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-    enable_format_on_save(client, bufnr)
-  end,
-  capabilities = capabilities,
-  settings = {
+  cssls = {},
+  html = {},
+  jsonls = {},
+  emmet_ls = {},
+  clangd = {},
+  rust_analyzer = {},
+  graphql = {},
+  tsserver = {},
+  sumneko_lua = {
     Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      },
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
     },
-  },
+  }
 }
 
+-- neodev must be loaded before lspconfig
+require('neodev').setup {}
 
+-- Mason must be loaded before lspconfig
+require('mason').setup {}
 
-lspconfig.graphql.setup {
-  capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    client.server_capabilities.hoverProvider = false
-    on_attach(client, bufnr)
+require('mason-lspconfig').setup {
+  automatic_installation = true,
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+require('mason-lspconfig').setup_handlers {
+  function(server_name)
+    lspconfig[server_name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = servers[server_name],
+    }
+  end,
+  graphql = function()
+    lspconfig.graphql.setup {
+      on_attach = function(client, bufnr)
+        client.server_capabilities.hoverProvider = false
+        on_attach(client, bufnr)
+      end,
+      capabilities = capabilities,
+      settings = servers.graphql,
+    }
+  end,
+  tsserver = function()
+    require('typescript').setup {
+      server = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = servers.tsserver,
+        init_options = {
+          plugins = {
+            {
+              name = 'typescript-styled-plugin',
+              location = '/Users/eduardo/.nvm/versions/node/v16.14.2/lib' -- Path to global node modules install location
+            },
+          },
+        },
+      }
+    }
+  end,
+  sumneko_lua = function()
+    lspconfig.sumneko_lua.setup {
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+
+        -- Format on save
+        local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+
+        vim.api.nvim_clear_autocmds({
+          group = augroup_format,
+          buffer = bufnr
+        })
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup_format,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end,
+      capabilities = capabilities,
+      settings = servers.sumneko_lua,
+    }
   end
 }
