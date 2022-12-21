54679b9ce2e3d5470c550d28e5085b16f3db6c31
Eduardo Vicente <eduardomcv42@gmail.com>
2022-12-21 23:26:54 +0000

    Add helper functions

--- neovim/.config/nvim/lua/eduardomcv/keymaps.lua
+++ neovim/.config/nvim/lua/eduardomcv/keymaps.lua
@@ -1,57 +1,65 @@
 local set = vim.keymap.set
 
+local function nmap(lhr, rhs)
+  set('n', lhr, rhs)
+end
+
+local function vmap(lhr, rhs)
+  set('v', lhr, rhs)
+end
+
 -- Set leader to spacebar
 vim.g.mapleader = ' '
 
 -- Delete without yank
-set('n', 'x', [["_x]])
+nmap('x', [["_x]])
 set({ 'n', 'v' }, '<leader>d', [["_d]])
 
 -- Paste without yank
 set('x', '<leader>p', [["_dP]])
 
 -- Keep cursor in place when joining lines
-set('n', 'J', 'mzJ`z')
+nmap('J', 'mzJ`z')
 
 -- Keep cursor in the middle when navigating
-set('n', '<C-d>', '<C-d>zz')
-set('n', '<C-u>', '<C-u>zz')
+nmap('<C-d>', '<C-d>zz')
+nmap('<C-u>', '<C-u>zz')
 
 -- Keep cursor in the middle when repeating searches
-set('n', 'n', 'nzzzv')
-set('n', 'N', 'Nzzzv')
+nmap('n', 'nzzzv')
+nmap('N', 'Nzzzv')
 
-set('n', 'Q', '<nop>')
+nmap('Q', '<nop>')
 
 -- Increment/decrement
-set('n', '+', '<C-a>')
-set('n', '-', '<C-x>')
+nmap('+', '<C-a>')
+nmap('-', '<C-x>')
 
 -- Select all
-set('n', '<C-a>', 'gg<S-v>G')
+nmap('<C-a>', 'gg<S-v>G')
 
 -- Move window
-set('n', '<leader>h', '<C-w>h')
-set('n', '<leader>k', '<C-w>k')
-set('n', '<leader>j', '<C-w>j')
-set('n', '<leader>l', '<C-w>l')
+nmap('<leader>h', '<C-w>h')
+nmap('<leader>k', '<C-w>k')
+nmap('<leader>j', '<C-w>j')
+nmap('<leader>l', '<C-w>l')
 
 -- Resize window
-set('n', '<C-w><left>', '<C-w><')
-set('n', '<C-w><right>', '<C-w>>')
-set('n', '<C-w><up>', '<C-w>+')
-set('n', '<C-w><down>', '<C-w>-')
+nmap('<C-w><left>', '<C-w><')
+nmap('<C-w><right>', '<C-w>>')
+nmap('<C-w><up>', '<C-w>+')
+nmap('<C-w><down>', '<C-w>-')
 
 -- Tab management
-set('n', '<leader>tn', ':tabnew<cr>')
-set('n', '<leader>to', ':tabonly<cr>')
-set('n', '<leader>tc', ':tabclose<cr>')
-set('n', '<leader>tm', ':tabmove<cr>')
-set('n', 'gj', ':tabnext<cr>')
-set('n', 'gk', ':tabprev<cr>')
+nmap('<leader>tn', ':tabnew<cr>')
+nmap('<leader>to', ':tabonly<cr>')
+nmap('<leader>tc', ':tabclose<cr>')
+nmap('<leader>tm', ':tabmove<cr>')
+nmap('gj', ':tabnext<cr>')
+nmap('gk', ':tabprev<cr>')
 
 -- alt-j and alt-k to move lines down and up
-set('n', '<a-j>', ':m+1<cr>')
-set('n', '<a-k>', ':m-2<cr>')
-set('v', '<a-j>', ":m '>+1<cr>gv=gv")
-set('v', '<a-k>', ":m '<-2<cr>gv=gv")
+nmap('<a-j>', ':m+1<cr>')
+nmap('<a-k>', ':m-2<cr>')
+vmap('<a-j>', ":m '>+1<cr>gv=gv")
+vmap('<a-k>', ":m '<-2<cr>gv=gv")
