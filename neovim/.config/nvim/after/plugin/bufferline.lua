62356ecb682144c4ba606c34b4c444b89d2ac80c
Eduardo Vicente <eduardomcv42@gmail.com>
2022-12-22 22:40:26 +0000

    Change tab cycle bindings. Remove unecessary configs. add diagnostics

--- neovim/.config/nvim/after/plugin/bufferline.lua
+++ neovim/.config/nvim/after/plugin/bufferline.lua
@@ -1,26 +1,16 @@
 local ok, bufferline = pcall(require, "bufferline")
 if not ok then return end
 
-local vs_colors = require('vscode.colors')
-
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
     always_show_bufferline = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
-  },
-  highlights = {
-    separator = {
-      fg = vs_colors.vscTabOutside,
-    },
-    separator_selected = {
-      fg = vs_colors.vscTabOutside
-    }
+    color_icons = true,
+    diagnostics = 'nvim_lsp',
   },
 })
 
-vim.keymap.set('n', '<leader>tk', ':BufferLineCycleNext<CR>', {})
-vim.keymap.set('n', '<leader>tj', ':BufferLineCyclePrev<CR>', {})
+vim.keymap.set('n', 'L', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', 'H', ':BufferLineCyclePrev<CR>')
