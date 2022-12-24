3fda2b46f48afa011e8e655335d4c7f923121206
Eduardo Vicente <eduardomcv42@gmail.com>
2022-12-24 00:45:39 +0000

    disable netrw and enable diagnostics

--- neovim/.config/nvim/after/plugin/nvim-tree.lua
+++ neovim/.config/nvim/after/plugin/nvim-tree.lua
@@ -2,6 +2,11 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  disable_netrw = true,
+  diagnostics = {
+    enable = true,
+  }
+})
 
 vim.keymap.set('n', '<leader>b', ':NvimTreeToggle<CR>')
