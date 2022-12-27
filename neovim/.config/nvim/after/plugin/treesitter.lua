e1ae8a75b20e064288fd4a8be5f2ea63806455cb
Eduardo Vicente <eduardo.vicente@blueprism.com>
2022-12-27 14:59:26 +0000

    Formatting. Add markdown

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+local ok, treesitter = pcall(require, 'nvim-treesitter.configs')
 if not ok then return end
 
 treesitter.setup {
@@ -11,23 +11,24 @@ treesitter.setup {
     disable = {},
   },
   ensure_installed = {
-    "help",
-    "c",
-    "lua",
-    "typescript",
-    "tsx",
-    "javascript",
-    "json",
-    "yaml",
-    "html",
-    "scss",
-    "css",
-    "graphql"
+    'help',
+    'c',
+    'lua',
+    'typescript',
+    'tsx',
+    'javascript',
+    'json',
+    'yaml',
+    'html',
+    'scss',
+    'css',
+    'graphql',
+    'markdown',
   },
   autotag = {
     enable = true
   }
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
