f892014573ad7730f4c8617cf6fc9f273ed8ddc3
Eduardo Vicente <eduardomcv42@gmail.com>
2022-12-30 17:20:15 +0000

    Remove node2. Add dap-vscode-js

--- neovim/.config/nvim/after/plugin/dap.lua
+++ neovim/.config/nvim/after/plugin/dap.lua
@@ -4,8 +4,16 @@ if not ok_dap then return end
 local ok_dapui, dapui = pcall(require, 'dapui')
 if not ok_dapui then return end
 
+local ok_dap_vscode_js, dap_vscode_js = pcall(require, 'dap-vscode-js')
+if not ok_dap_vscode_js then return end
+
 local utils = require('eduardomcv.utils')
 local mason_path = vim.fn.stdpath('data') .. '/mason'
+
+dap_vscode_js.setup({
+  adapters = { 'pwa-node' }, -- which adapters to register in nvim-dap
+  debugger_path = mason_path .. '/packages/js-debug-adapter'
+})
 
 dap.adapters.firefox = {
   type = 'executable',
@@ -13,13 +21,42 @@ dap.adapters.firefox = {
   args = { mason_path .. '/packages/firefox-debug-adapter/dist/adapter.bundle.js' },
 }
 
-dap.adapters.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = { mason_path .. '/packages/node-debug2-adapter/out/src/nodeDebug.js' },
+dap.configurations.typescript = {
+  {
+    name = "Debug Jest tests",
+    type = "pwa-node",
+    request = "launch",
+    trace = true, -- include debugger info
+    runtimeExecutable = "node",
+    runtimeArgs = {
+      "./node_modules/jest/bin/jest.js",
+      "--runInBand",
+    },
+    rootPath = "${workspaceFolder}",
+    cwd = "${workspaceFolder}",
+    console = "integratedTerminal",
+    internalConsoleOptions = "neverOpen",
+    sourceMaps = true,
+  }
 }
 
 dap.configurations.typescriptreact = {
+  {
+    name = "Debug Jest tests",
+    type = "pwa-node",
+    request = "launch",
+    trace = true, -- include debugger info
+    runtimeExecutable = "node",
+    runtimeArgs = {
+      "./node_modules/jest/bin/jest.js",
+      "--runInBand",
+    },
+    rootPath = "${workspaceFolder}",
+    cwd = "${workspaceFolder}",
+    console = "integratedTerminal",
+    internalConsoleOptions = "neverOpen",
+    sourceMaps = true,
+  },
   {
     name = "Attach Firefox",
     type = "firefox",
@@ -37,8 +74,10 @@ dap.configurations.typescriptreact = {
 
 dapui.setup()
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open {}
+-- We need to wait for execution to stop at the first breakpoint before showing the UI to give the source maps time to generate.
+-- If we don't, the UI will close because the source maps haven't generated in time.
+dap.listeners.after.event_breakpoint["dapui_config"] = function()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
   dapui.close {}
