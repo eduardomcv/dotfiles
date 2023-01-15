189445f953098c6ac9d885c637c419a2d112dfda
Eduardo Vicente <eduardomcv42@gmail.com>
2023-01-15 00:16:40 +0000

    Filter methods and fields

--- neovim/.config/nvim/after/plugin/cmp.lua
+++ neovim/.config/nvim/after/plugin/cmp.lua
@@ -78,12 +78,22 @@ cmp.setup({
     { name = 'luasnip' },
     {
       name = 'nvim_lsp',
-      entry_filter = function(entry)
+      entry_filter = function(entry, context)
         local kind = entry:get_kind()
         local node_at_cursor = ts_utils.get_node_at_cursor()
 
         if node_at_cursor:type() == 'arguments' then
           return kind == 6
+        end
+
+        local line = context.cursor_line
+        local col = context.cursor.col
+        local char_before_cursor = string.sub(line, col - 1, col - 1)
+
+        if char_before_cursor == '.' then
+          return kind == 2 or kind == 5
+        elseif string.match(line, '^%s*%w*$') then
+          return kind == 3 or kind == 6
         end
 
         return true
