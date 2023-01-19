741df75d7feab36832764a7abc57dedf99ec0d4a
Eduardo Vicente <eduardomcv42@gmail.com>
2023-01-19 20:59:55 +0000

    Add exit codes

--- utils/sudo.sh
+++ utils/sudo.sh
@@ -1,13 +1,13 @@
 check_is_sudo() {
   if [ "$(id -u)" -ne 0 ]; then
     echo "Please run as root."
-    exit
+    exit 1
   fi
 }
 
 check_is_not_sudo() {
   if [ "$(id -u)" -eq 0 ]; then
     echo "Please run as non-root."
-    exit
+    exit 1
   fi
 }
