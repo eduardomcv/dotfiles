8c4b03ad2fa136d1a9551ac4e975756b13247cb9
Eduardo Vicente <eduardomcv42@gmail.com>
2023-01-26 23:11:24 +0000

    Setup lspsaga colors

--- neovim/.config/nvim/after/plugin/lspsaga.lua
+++ neovim/.config/nvim/after/plugin/lspsaga.lua
@@ -1,19 +1,27 @@
 local ok_lspsaga, lspsaga = pcall(require, "lspsaga")
 if not ok_lspsaga then return end
 
-local ok_colors, colors = pcall(require, 'vscode.colors')
+local ok_colors, colors = pcall(require, 'tokyonight.colors')
 if not ok_colors then return end
 
 local utils = require('user.utils')
-local c = colors.get_colors()
+local c = colors.setup({ style = 'night' })
 
 lspsaga.setup({
   ui = {
     border = 'rounded',
     colors = {
-      normal_bg = c.vscPopupBack,
-      magenta = c.vscPink,
-      cyan = c.vscLightBlue,
+      normal_bg = c.bg_dark,
+      red = c.red,
+      magenta = c.magenta,
+      orange = c.orange,
+      yellow = c.yellow,
+      green = c.green,
+      cyan = c.cyan,
+      blue = c.blue,
+      purple = c.purple,
+      white = '#d1d4cf',
+      black = '#1c1c19',
     }
   }
 })
