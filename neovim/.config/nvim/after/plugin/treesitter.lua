7dd9b5cf9b9954f7e2d4c2ac8f83eecaa39cc809
Eduardo Vicente <eduardomcv42@gmail.com>
2023-01-30 23:02:52 +0000

    Update and cleanup treesitter config

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -4,31 +4,26 @@ if not ok then return end
 treesitter.setup {
   highlight = {
     enable = true,
-    disable = {},
   },
   indent = {
     enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    'help',
-    'c',
-    'lua',
-    'typescript',
-    'tsx',
-    'javascript',
-    'json',
-    'yaml',
-    'html',
-    'scss',
-    'css',
-    'graphql',
-    'markdown',
   },
   autotag = {
     enable = true
-  }
+  },
+  ensure_installed = {
+    'help',
+    'lua',
+    'javascript',
+    'typescript',
+    'tsx',
+    'html',
+    'css',
+    'scss',
+    'graphql',
+    'json',
+    'yaml',
+    'markdown',
+    'markdown_inline',
+  },
 }
-
-local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
