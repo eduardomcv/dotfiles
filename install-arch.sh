173d0b39ed86bf9f1a089483958296c59821d88e
Eduardo Vicente <eduardomcv42@gmail.com>
2023-04-10 14:32:35 +0100

    first draft of arch install script

---
+++ install-arch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source ./utils/sudo.sh
+
+check_is_sudo
+
+pacman -Syu --noconfirm \
+  base-devel \
+  man-db \
+  git \
+  stow \
+  curl \
+  wget \
+  i3-wm \
+  i3lock \
+  polybar \
+  rofi \
+  ttf-firacode-nerd \
+  adobe-source-han-sans-otc-fonts \
+  adobe-source-han-serif-otc-fonts \
+  blueman \
+  networkmanager \
+  network-manager-applet \
+  lxsession-gtk3 \
+  dunst \
+  maim \
+  htop \
+  imagemagick \
+  feh \
+  zsh \
+  zip \
+  unzip \
+  starship \
+  ripgrep \
+  bat \
+  fd \
+  exa \
+  fzf \
+  lazygit \
+  neovim \
+  xclip \
+  firefox \
+  wezterm \
+  thunar \
+  thunar-archive-plugin \
+  thunar-media-tags-plugin \
+  thunar-volman \
+  ristretto \
+  vlc \
+  mate-calc \
+  flatpak \
+  steam \
+  discord
