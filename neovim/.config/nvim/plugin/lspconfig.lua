eb0e0a7d00453a21f5c3d2a6b8a1665a61866646
Eduardo Vicente <eduardomcv42@gmail.com>
2023-04-18 14:46:33 +0100

    add vim global

--- neovim/.config/nvim/plugin/lspconfig.lua
+++ neovim/.config/nvim/plugin/lspconfig.lua
@@ -113,6 +113,9 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
+      diagnostics = {
+        globals = { 'vim' },
+      },
     },
   }
 }
