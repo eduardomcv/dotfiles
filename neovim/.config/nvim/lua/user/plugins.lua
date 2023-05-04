d5f35003932dbe14120ec23f008c176a73b3737b
Eduardo Vicente <eduardomcv42@gmail.com>
2023-05-04 17:33:45 +0100

    add tmux plugin

--- neovim/.config/nvim/lua/user/plugins.lua
+++ neovim/.config/nvim/lua/user/plugins.lua
@@ -127,9 +127,10 @@ return packer.startup(function(use)
     'folke/trouble.nvim',                     -- Pretty diagnostics
     requires = 'nvim-tree/nvim-web-devicons',
   }
-  use 'yorickpeterse/nvim-pqf' -- Pretty quickfix list
+  use 'yorickpeterse/nvim-pqf'         -- Pretty quickfix list
+  use 'christoomey/vim-tmux-navigator' -- tmux navigation
 
-  use 'github/copilot.vim'     -- Copilot
+  use 'github/copilot.vim'             -- Copilot
 
   if packer_bootstrap then
     packer.sync()
