0addfd990144e24c869f7cea5e28ed960112916c
Eduardo Vicente <eduardomcv42@gmail.com>
2023-05-08 14:28:48 +0100

    add set variable

--- neovim/.config/nvim/lua/user/base.lua
+++ neovim/.config/nvim/lua/user/base.lua
@@ -1,7 +1,9 @@
+local set = vim.opt
+
 vim.cmd("autocmd!")
 
 -- Ignore files
-vim.opt.wildignore:append {
+set.wildignore:append {
   '**/node_modules/*',
   '**/.git/*',
   '**/.vscode/*',
@@ -10,85 +12,85 @@ vim.opt.wildignore:append {
 }
 
 -- Encoding
-vim.opt.encoding = 'utf-8'
+set.encoding = 'utf-8'
 vim.scriptencoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+set.fileencoding = 'utf-8'
 
 -- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+set.number = true
+set.relativenumber = true
 
 -- Indentation
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.breakindent = true
-vim.opt.smarttab = true
+set.shiftwidth = 2
+set.tabstop = 2
+set.softtabstop = 2
+set.expandtab = true
+set.smartindent = true
+set.breakindent = true
+set.smarttab = true
 
 -- Highlights
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
-vim.opt.pumblend = 5
-vim.opt.background = 'dark'
+set.cursorline = true
+set.termguicolors = true
+set.winblend = 0
+set.wildoptions = 'pum'
+set.pumblend = 5
+set.background = 'dark'
 
 
 -- Don't keep searched terms highlighted
-vim.opt.hlsearch = false
+set.hlsearch = false
 
 -- Ignore case when searching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+set.ignorecase = true
+set.smartcase = true
 
 -- Min 10 Lines to the cursor
-vim.opt.scrolloff = 10
+set.scrolloff = 10
 
 -- Vertical line at 120 chars
-vim.opt.colorcolumn = '120'
+set.colorcolumn = '120'
 
 -- Don't break in the middle of a word
-vim.opt.linebreak = true
+set.linebreak = true
 
 -- Nice menu for :find
-vim.opt.wildmode = { 'longest', 'list', 'full' }
+set.wildmode = { 'longest', 'list', 'full' }
 
 -- Don't show mode on last line
-vim.opt.showmode = false
+set.showmode = false
 
 -- Always show sign column
-vim.opt.signcolumn = 'yes'
+set.signcolumn = 'yes'
 
 -- Set the terminal title at will
-vim.opt.title = true
+set.title = true
 
 -- Split right on new windows
-vim.opt.splitright = true
+set.splitright = true
 
 -- Setup undo file
-vim.opt.undofile = true
+set.undofile = true
 
 -- Don't use swap or backups
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.writebackup = false
+set.swapfile = false
+set.backup = false
+set.writebackup = false
 
 -- Mouse support
-vim.opt.mouse = 'a'
+set.mouse = 'a'
 
 -- Set shell to zsh
-vim.opt.shell = 'zsh'
+set.shell = 'zsh'
 
-vim.opt.inccommand = 'split'
-vim.opt.backspace = { 'start', 'eol', 'indent' }
+set.inccommand = 'split'
+set.backspace = { 'start', 'eol', 'indent' }
 
 -- Finding files - Search down into subfolders
-vim.opt.path:append { '**' }
+set.path:append { '**' }
 
 -- Auto insert comment leader on enter
-vim.opt.formatoptions:append { 'r' }
+set.formatoptions:append { 'r' }
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
