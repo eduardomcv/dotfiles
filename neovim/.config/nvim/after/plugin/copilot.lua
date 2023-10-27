acd62358f79941d847e84b1e0a219aa88d776367
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-27 13:37:52 +0100

    disable copilot

--- neovim/.config/nvim/after/plugin/copilot.lua
+++ neovim/.config/nvim/after/plugin/copilot.lua
@@ -1,17 +1,17 @@
-local ok, copilot = pcall(require, 'copilot')
-if not ok then return end
-
-copilot.setup {
-  suggestion = {
-    auto_trigger = true,
-    keymap = {
-      accept = '<C-j>',
-    },
-  },
-  filetypes = {
-    ['*'] = false,
-    javascript = true,
-    typescript = true,
-    typescriptreact = true
-  }
-}
+-- local ok, copilot = pcall(require, 'copilot')
+-- if not ok then return end
+--
+-- copilot.setup {
+--   suggestion = {
+--     auto_trigger = true,
+--     keymap = {
+--       accept = '<C-j>',
+--     },
+--   },
+--   filetypes = {
+--     ['*'] = false,
+--     javascript = true,
+--     typescript = true,
+--     typescriptreact = true
+--   }
+-- }
