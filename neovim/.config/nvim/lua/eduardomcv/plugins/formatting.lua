91d911ac285e6d830c608fd6a97a465c090f39df
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-29 13:30:48 +0000

    add formatting plugin

---
+++ neovim/.config/nvim/lua/eduardomcv/plugins/formatting.lua
@@ -0,0 +1,36 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local conform = require('conform')
+
+    conform.setup {
+      formatters_by_ft = {
+        javascript = { 'prettierd' },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        graphql = { "prettierd" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    }
+
+    -- Keymaps
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end)
+  end
+}
