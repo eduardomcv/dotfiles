d91d949799d758865bdd260bfdd6bcc07498d94e
Eduardo Vicente <eduardomcv42@gmail.com>
2023-10-29 14:07:30 +0000

    add mason tool installer

--- neovim/.config/nvim/lua/eduardomcv/plugins/lsp/mason.lua
+++ neovim/.config/nvim/lua/eduardomcv/plugins/lsp/mason.lua
@@ -2,6 +2,7 @@ return {
   "williamboman/mason.nvim", -- Manage LSPs, linters, formatters
   dependencies = {
     "williamboman/mason-lspconfig.nvim", -- Make it easier to use lspconfig with mason
+    "WhoIsSethDaniel/mason-tool-installer.nvim", -- Auto install linters, formatters and other tools
   },
   config = function()
     local servers = require("eduardomcv.plugins.lsp.config.server-configurations")
@@ -11,6 +12,14 @@ return {
     require("mason-lspconfig").setup({
       automatic_installation = true,
       ensure_installed = vim.tbl_keys(servers),
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettierd",
+        "eslint_d",
+        "stylua",
+      },
     })
   end,
 }
