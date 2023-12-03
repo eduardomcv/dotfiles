062406dba8ef4ff0ac295738639304f12d80c5bd
Eduardo Vicente <eduardomcv42@gmail.com>
2023-12-03 19:45:53 +0000

    add keymaps and autocmds

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set *.mdx filetype to markdown
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.mdx",
+  command = "set filetype=markdown",
+})
