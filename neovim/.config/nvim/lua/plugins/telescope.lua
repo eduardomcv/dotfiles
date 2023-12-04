52b7a9660b88adbdbf70f9b3ef2fff6b67ae54f7
Eduardo Vicente <eduardomcv42@gmail.com>
2023-12-04 11:27:38 +0000

    remove unused function

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,3 @@
-local function getVisualSelection()
-  vim.cmd('noau normal! "vy"')
-  local text = vim.fn.getreg("v")
-  vim.fn.setreg("v", {})
-
-  text = string.gsub(text, "\n", "")
-  if #text > 0 then
-    return text
-  else
-    return ""
-  end
-end
-
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
