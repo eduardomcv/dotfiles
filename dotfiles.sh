92c10f0a57c48eb85d5bef3a348a0cf791a73a7c
Eduardo Vicente <eduardomcv42@gmail.com>
2023-12-06 17:07:23 +0000

    add sway and wofi configs

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,23 +6,23 @@ source ./utils/sudo.sh
 
 check_is_not_sudo
 
-THINGS_TO_STOW=( \
-    git \
-    zsh \
-    neovim \
-    tmux \
-    wezterm
+THINGS_TO_STOW=(
+	git
+	zsh
+	neovim
+	tmux
+	wezterm
+	sway
 )
 
 for thing in ${THINGS_TO_STOW[@]}; do
-    if [[ "$#" == 0 || "$1" == "uninstall" ]]; then
-        stow -vDt ~ $thing
-    fi
+	if [[ "$#" == 0 || "$1" == "uninstall" ]]; then
+		stow -vDt ~ $thing
+	fi
 
-    if [[ "$#" == 0 || $1 == "install" ]]; then
-        stow -vt ~ $thing
-    fi
+	if [[ "$#" == 0 || $1 == "install" ]]; then
+		stow -vt ~ $thing
+	fi
 done
 
 echo Done.
-
