31ce3812fc8167377f3901ecee51274d912496ef
Eduardo Vicente <eduardomcv42@gmail.com>
2024-04-16 09:51:24 +0100

    comment unecessary plugins

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -2,8 +2,8 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
-      "vue-language-server",
-      "svelte-language-server",
+      -- "vue-language-server",
+      -- "svelte-language-server",
     },
   },
 }
