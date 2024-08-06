a5fb4bc1bee6d452a4de051b52fcf0785db36a1c
Eduardo Vicente <eduardomcv42@gmail.com>
2024-08-06 10:39:33 +0100

    remove old directory

--- old/i3/.config/i3/lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-maim -f png /tmp/lock.png
-convert /tmp/lock.png -scale 10% -scale 1000% /tmp/lock.png
-[[ -f $1 ]] && convert /tmp/lock.png $1 -gravity center -composite -matte /tmp/lock.png
-i3lock -i /tmp/lock.png
-rm /tmp/lock.png
