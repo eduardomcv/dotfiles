38ca21af6548e6d210b5a5a80e4f21083e1c1f34
Eduardo Vicente <eduardomcv42@gmail.com>
2024-10-28 16:23:46 +0000

    add cssls and stylelint_lsp configs

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -2,5 +2,15 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      cssls = {},
+      stylelint_lsp = {
+        settings = {
+          stylelintplus = {
+            autoFixOnSave = true,
+          },
+        },
+      },
+    },
   },
 }
