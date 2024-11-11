0cff2b22a71e838d3f3570e522c486a10d1c6475
Eduardo Vicente <eduardomcv42@gmail.com>
2024-11-11 11:36:27 +0000

    remove supertab

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -4,48 +4,11 @@ return {
   opts = function(_, opts)
     local cmp = require("cmp")
 
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
-    local function next_item(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.snippet.active({ direction = 1 }) then
-        vim.schedule(function()
-          vim.snippet.jump(1)
-        end)
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end
-
-    local function prev_item(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.snippet.active({ direction = -1 }) then
-        vim.schedule(function()
-          vim.snippet.jump(-1)
-        end)
-      else
-        fallback()
-      end
-    end
-
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping(next_item, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(prev_item, { "i", "s" }),
-      ["<C-d>"] = cmp.mapping.scroll_docs(4),
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      }),
+      ["<C-d>"] = cmp.mapping.scroll_docs(4),
     })
 
     opts.window = vim.tbl_extend("force", opts.window or {}, {
