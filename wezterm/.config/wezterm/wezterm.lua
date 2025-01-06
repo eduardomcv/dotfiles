d94202780ef8a2d62d0a59471e25683e9d8b9c84
Eduardo Vicente <eduardomcv42@gmail.com>
2025-01-06 12:49:14 +0000

    add util to check os and add homebrew binaries on macos

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,4 +14,39 @@ local config = {
 	},
 }
 
+-- An util to check which OS wezterm is running on.
+local function detect_host_os()
+	-- package.config:sub(1,1) returns '\' for windows and '/' for *nix.
+	if package.config:sub(1, 1) == "\\" then
+		return "windows"
+	else
+		local result = "unknown"
+
+		-- uname should be available on *nix systems.
+		local handler = io.popen("uname -s")
+
+		if handler ~= nil then
+			result = handler:read("*l")
+			handler:close()
+		end
+
+		if result == "unknown" then
+			return "unknown"
+		elseif result == "Darwin" then
+			return "macos"
+		else
+			return "linux"
+		end
+	end
+end
+
+local host_os = detect_host_os()
+
+-- add homebrew binaries to $PATH on macos
+if host_os == "macos" then
+	config.set_environment_variables = {
+		PATH = "/opt/homebrew/bin/:" .. os.getenv("PATH"),
+	}
+end
+
 return config
