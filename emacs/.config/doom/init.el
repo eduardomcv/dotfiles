3887ca54cc89010c4a0f353776a93e41b7189197
Eduardo Vicente <eduardomcv42@gmail.com>
2025-01-08 22:24:50 +0000

    change wezterm font size and remove doom modules

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -103,13 +103,13 @@
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;terraform         ; infrastructure as code
-       tmux              ; an API for interacting with tmux
+       ;;tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
        (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
-       tty               ; improve the terminal Emacs experience
+       ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
