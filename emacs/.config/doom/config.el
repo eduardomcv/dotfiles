932dceb161ded228b12af6b0e36c6fffc9871da7
Eduardo Vicente <eduardomcv42@gmail.com>
2025-01-10 16:03:41 +0000

    fix catppuccin theme not being loaded

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -36,8 +36,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
+(setq doom-theme nil)
 (load-theme 'catppuccin :no-confirm)
+
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -45,7 +46,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+;; (setq org-directory "~/org")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
