1ecc5b13b49fe32e34caaac25f1011bec4423480
Eduardo Vicente <eduardomcv42@gmail.com>
2025-01-18 23:45:52 +0000

    remove annoying keymaps

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -4,17 +4,6 @@ local set = vim.keymap.set
 set("n", "x", [["_x]], { desc = "Delete without yanking" })
 set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
 
--- Keep cursor in place when joining lines
-set("n", "J", "mzJ`z")
-
--- Keep cursor in the middle when navigating
-set("n", "<C-d>", "<C-d>zz")
-set("n", "<C-u>", "<C-u>zz")
-
--- Keep cursor in the middle when repeating searches
-set("n", "n", "nzzzv")
-set("n", "N", "Nzzzv")
-
 -- Increment/decrement
 set("n", "+", "<C-a>")
 set("n", "-", "<C-x>")
