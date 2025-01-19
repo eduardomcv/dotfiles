0f56a4c3cc03d0a4155d030499bd991df107627a
Eduardo Vicente <eduardomcv42@gmail.com>
2025-01-19 00:36:40 +0000

    add formatter maps and keybinds

--- neovim/.config/nvim/lua/plugins/formatting.lua
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -10,12 +10,23 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
 			},
 			default_format_opts = {
 				lsp_format = "fallback",
 			},
 			format_on_save = {
 				timeout_ms = 500,
+			},
+		},
+		keys = {
+			{
+				"<leader>cf",
+				function()
+					require("conform").format({ async = true })
+				end,
+				desc = "Format buffer",
 			},
 		},
 	},
