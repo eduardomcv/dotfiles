ee2722ac31e7205be738509abaddd2164bd64857
Eduardo Vicente <eduardomcv42@gmail.com>
2025-01-19 01:05:46 +0000

    add surround plugins and add modules to treesitter

--- neovim/.config/nvim/lua/plugins/code.lua
+++ neovim/.config/nvim/lua/plugins/code.lua
@@ -53,10 +53,29 @@ return {
 		build = ":TSUpdate",
 		event = "VeryLazy",
 		config = function()
+			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+				textobjects = {
+					move = {
+						enable = true,
+						goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+						goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+						goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+						goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+					},
+				},
 				ensure_installed = {
 					"bash",
 					"tmux",
@@ -94,5 +113,11 @@ return {
 	{
 		"windwp/nvim-ts-autotag",
 		deps = "nvim-treesitter",
+	},
+	-- Surround functionality
+	{
+		"echasnovski/mini.surround",
+		version = "*",
+		config = true,
 	},
 }
