f8843f714ef70f44796fd7de37aa9a2eb1534d50
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-05-23 22:04:09 +0100

    add lazygit install script

--- install.sh
+++ install.sh
@@ -24,6 +24,12 @@ sudo apt install -y \
   eza \
   tmux
 
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
 # Add nvim PPA
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
