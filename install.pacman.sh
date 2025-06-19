7ddcb374f93de4000bdc52ef7b79c32e0d1f3127
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-06-19 03:31:24 +0100

    add pacman install script

---
+++ install.pacman.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source ./utils/sudo.sh
+
+check_is_not_sudo
+
+# update system
+sudo pacman -Syu
+
+# install packages
+sudo pacman -S --noconfirm \
+  base-devel \
+  git \
+  stow \
+  zsh \
+  fd \
+  ripgrep \
+  fzf \
+  bat \
+  eza \
+  tmux \
+  lazygit \
+  neovim \
+	ttf-jetbrains-mono
+
+# Create directory for user binaries
+mkdir -p ~/.local/bin
+
+# Install dotfiles
+echo Installing dotfiles...
+./dotfiles.sh
+
+echo Done.
