a103fa00b6ab8801a9d9ba9ec27f841cbc8a83e2
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-06-19 03:30:57 +0100

    remove wezterm and cmake from install script and use make to compile fzf native

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y \
   build-essential \
   software-properties-common \
-	cmake \
   git \
   stow \
   zsh \
@@ -33,18 +32,13 @@ sudo install lazygit -D -t /usr/local/bin/
 # Add nvim PPA
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
-# Add wezterm repository
-curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
-echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
-sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
-
 # Add Spotify repository
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # Install packages
 sudo apt update
-sudo apt install -y neovim wezterm spotify-client
+sudo apt install -y neovim spotify-client
 
 # Create directory for user binaries
 mkdir -p ~/.local/bin
