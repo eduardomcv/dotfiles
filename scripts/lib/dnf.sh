3b42e6f56b35564f6a5c2b652727df698f85bcd2
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-07-11 04:12:06 +0100

    fix dnf script

--- scripts/lib/dnf.sh
+++ scripts/lib/dnf.sh
@@ -8,7 +8,7 @@ check_rpmfusion() {
 		# openh264 needs to be explicitly enabled
 		sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
-		sudo dnf install \
+		sudo dnf install -y \
 			https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 			https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 	fi
@@ -19,36 +19,24 @@ enable_copr() {
 	# yes, it has do be one command at a time.
 	# considering putting this on a loop and read repo names from a txt file.
 	sudo dnf copr enable -y atim/lazygit
-	sudo dnf copr enable -y pgdev/ghostty
 	sudo dnf copr enable -y alternateved/eza
 }
 
 install_dnf() {
 	# update system
-	sudo dnf update -y
-
-	check_rpmfusion
-
-	enable_copr
-
-	# install packages
-	sudo dnf install -y \
-		@development-tools \
-		stow \
-		lua \
-		lua5.1 \
-		luarocks \
-		jetbrains-mono-fonts-all \
-		fd-find \
-		ripgrep \
-		bat \
-		eza \
-		fzf \
-		zsh \
-		tmux \
-		lazygit \
-		neovim \
-		ghostty \
-		firefox \
-		thunderbird
+	sudo dnf update -y \
+		&& check_rpmfusion \
+		&& enable_copr \
+		&& sudo dnf install -y \
+			@development-tools \
+			stow \
+			fd-find \
+			ripgrep \
+			bat \
+			eza \
+			fzf \
+			zsh \
+			tmux \
+			lazygit \
+			neovim
 }
