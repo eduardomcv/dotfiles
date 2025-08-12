8a66c64e3566cf0a27ca3562019c73635166dd0f
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-08-12 20:35:45 +0100

    split mini and snacks plugins and add single mini plugin

---
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,61 @@
+return {
+	"echasnovski/mini.nvim",
+	version = false,
+	config = function()
+		-- Icons
+		require("mini.icons").setup()
+		MiniIcons.mock_nvim_web_devicons()
+
+		-- File explorer
+		require("mini.files").setup({
+			content = {
+				-- Filter these files from the explorer
+				filter = function(entry)
+					return entry.name ~= ".DS_Store" and entry.name ~= ".git" and entry.name ~= ".direnv"
+				end,
+			},
+		})
+
+		-- Tracking of git data and exposes :Git command
+		require("mini.git").setup()
+
+		-- Integrates git diffs and hunks
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+			},
+		})
+
+		-- Status line
+		require("mini.statusline").setup()
+
+		-- Extends forward/back functionality with brackets
+		require("mini.bracketed").setup()
+
+		-- Add gS keybind to split/join arguments
+		require("mini.splitjoin").setup()
+	end,
+	keys = {
+		{
+			"<leader>e",
+			function()
+				local MiniFiles = require("mini.files")
+				if not MiniFiles.close() then
+					MiniFiles.open(vim.api.nvim_buf_get_name(0))
+					MiniFiles.reveal_cwd()
+				end
+			end,
+			desc = "Toggle file explorer (current buffer)",
+		},
+		{
+			"<leader>r",
+			function()
+				local MiniFiles = require("mini.files")
+				if not MiniFiles.close() then
+					MiniFiles.open()
+				end
+			end,
+			desc = "Toggle file explorer (root dir)",
+		},
+	},
+}
