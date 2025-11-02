b17993c235a0a0cd6a13bb50f00a87cb7954b112
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-11-02 01:09:53 +0000

    add new script

---
+++ scripts/install-npm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run this after installing Node.js through nvm.
+# Installs dependencies and language servers for neovim
+
+set -euo pipefail
+
+if ! command -v npm >/dev/null 2>&1; then
+	echo "npm is not installed!"
+	exit 1
+fi
+
+npm install -g \
+	prettier \
+	@vtsls/language-server \
+	@olrtg/emmet-language-server \
+	eslint-lsp \
+	bash-language-server \
+	vscode-langservers-extracted
