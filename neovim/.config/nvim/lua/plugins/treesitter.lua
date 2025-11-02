d9cdcf6761101a45979fe6543badf91e9f6c473b
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-11-02 00:08:15 +0000

    add ensure installed

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -25,6 +25,14 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 	highlight = { enable = true },
 	indent = { enable = true },
+	ensure_installed = {
+		"bash",
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"regex",
+		"vim",
+	},
 	incremental_selection = {
 		enable = true,
 		keymaps = {
