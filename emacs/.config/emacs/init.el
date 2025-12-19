688776e7b8a64204fb4051b69e75b3e48fe4f0ac
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-12-19 11:50:17 +0000

    improve configuration

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -46,7 +46,7 @@
 
   ;; Store backups file in var/
   (setq backup-directory-alist
-      `(("." . ,(no-littering-expand-var-file-name "backups/"))))
+	`(("." . ,(no-littering-expand-var-file-name "backups/"))))
   ;; Store auto-save files in var/
   (setq auto-save-file-name-transforms
         `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
@@ -59,24 +59,23 @@
   (load-theme 'catppuccin :no-confirm))
 
 ;;; Fonts
-
-(set-face-attribute 'default nil :font "JetBrainsMono" :height 140 :weight 'regular)
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono-14"))
 
 (use-package ligature
   :config
   (ligature-set-ligatures 'prog-mode '("--" "---" "==" "===" "!=" "!==" "=!="
-                              "=:=" "=/=" "<=" ">=" "&&" "&&&" "&=" "++" "+++" "***" ";;" "!!"
-                              "??" "???" "?:" "?." "?=" "<:" ":<" ":>" ">:" "<:<" "<>" "<<<" ">>>"
-                              "<<" ">>" "||" "-|" "_|_" "|-" "||-" "|=" "||=" "##" "###" "####"
-                              "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:" "#!" "#=" "^=" "<$>" "<$"
-                              "$>" "<+>" "<+" "+>" "<*>" "<*" "*>" "</" "</>" "/>" "<!--" "<#--"
-                              "-->" "->" "->>" "<<-" "<-" "<=<" "=<<" "<<=" "<==" "<=>" "<==>"
-                              "==>" "=>" "=>>" ">=>" ">>=" ">>-" ">-" "-<" "-<<" ">->" "<-<" "<-|"
-                              "<=|" "|=>" "|->" "<->" "<~~" "<~" "<~>" "~~" "~~>" "~>" "~-" "-~"
-                              "~@" "[||]" "|]" "[|" "|}" "{|" "[<" ">]" "|>" "<|" "||>" "<||"
-                              "|||>" "<|||" "<|>" "..." ".." ".=" "..<" ".?" "::" ":::" ":=" "::="
-                              ":?" ":?>" "//" "///" "/*" "*/" "/=" "//=" "/==" "@_" "__" "???"
-                              "<:<" ";;;"))
+				       "=:=" "=/=" "<=" ">=" "&&" "&&&" "&=" "++" "+++" "***" ";;" "!!"
+				       "??" "???" "?:" "?." "?=" "<:" ":<" ":>" ">:" "<:<" "<>" "<<<" ">>>"
+				       "<<" ">>" "||" "-|" "_|_" "|-" "||-" "|=" "||=" "##" "###" "####"
+				       "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:" "#!" "#=" "^=" "<$>" "<$"
+				       "$>" "<+>" "<+" "+>" "<*>" "<*" "*>" "</" "</>" "/>" "<!--" "<#--"
+				       "-->" "->" "->>" "<<-" "<-" "<=<" "=<<" "<<=" "<==" "<=>" "<==>"
+				       "==>" "=>" "=>>" ">=>" ">>=" ">>-" ">-" "-<" "-<<" ">->" "<-<" "<-|"
+				       "<=|" "|=>" "|->" "<->" "<~~" "<~" "<~>" "~~" "~~>" "~>" "~-" "-~"
+				       "~@" "[||]" "|]" "[|" "|}" "{|" "[<" ">]" "|>" "<|" "||>" "<||"
+				       "|||>" "<|||" "<|>" "..." ".." ".=" "..<" ".?" "::" ":::" ":=" "::="
+				       ":?" ":?>" "//" "///" "/*" "*/" "/=" "//=" "/==" "@_" "__" "???"
+				       "<:<" ";;;"))
   (global-ligature-mode t))
 
 ;;; UI
@@ -93,10 +92,10 @@
 (hl-line-mode 1)
 
 ;; Consistent relative line numbers
+(setq-default display-line-numbers-type 'relative)
+(setq-default display-line-numbers-width 3)
+(setq-default display-line-numbers-grow-only t)
 (global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
-(setq display-line-numbers-width 4)
-(setq display-line-numbers-grow-only t)
 
 ;; Enable which key
 (which-key-mode 1)
@@ -116,8 +115,8 @@
   :config
   (vertico-posframe-mode 1)
   (setq vertico-posframe-parameters
-        '((left-fringe . 8)
-          (right-fringe . 8)))
+	'((left-fringe . 8)
+	  (right-fringe . 8)))
 
   (setq vertico-posframe-border-width 2)
 
@@ -125,13 +124,29 @@
   ;;(setq vertico-posframe-poshandler 'vertico-posframe-poshandler-frame-center)
   
   ;; 4. (Optional) Make it slightly smaller/wider to match your taste
-  (setq vertico-posframe-width 80)
-  (setq vertico-posframe-height 20))
+  (setq vertico-posframe-width 90)
+  (setq vertico-posframe-height 25))
 
 ;; Rich annotations
 (use-package marginalia
   :init
   (marginalia-mode))
+
+;; Highlight TODO comments
+(use-package hl-todo
+  :hook ((prog-mode . hl-todo-mode)
+         (text-mode . hl-todo-mode))
+  :config
+  (setq hl-todo-highlight-punctuation ":")
+  ;; Keywords
+  (setq hl-todo-keyword-faces
+	'(
+          ("TODO"     . "#96CDFB")
+	  ("FIXME"       . "#F28FAD")
+          ("HACK"       . "#FAE3B0")
+          ("DEPRECATED"      . "#E8A2AF")
+          ("NOTE"       . "#ABE9B3"))))
+
 
 ;;; Evil mode
 
@@ -150,6 +165,11 @@
   :config
   (evil-collection-init))
 
+(use-package evil-commentary
+  :after evil
+  :config
+  (evil-commentary-mode 1))
+
 ;;; Git
 
 (use-package magit
@@ -160,25 +180,41 @@
 ;;; Fuzzy finder
 (use-package consult
   :bind (
-    ("C-c g" . consult-ripgrep)
-    ("C-c f" . consult-fd)
-    ("C-c b" . consult-buffer)
-    ("C-c s" . consult-line)
-  )
+	 ("C-c g" . consult-ripgrep)
+	 ("C-c f" . consult-fd)
+	 ("C-c b" . consult-buffer)
+	 ("C-c s" . consult-line)
+	 )
   :config
   (setq consult-async-min-input 0)
   (let ((fd-name (if (executable-find "fdfind") "fdfind" "fd")))
-    (setq consult-fd-args (concat fd-name " --full-path --absolute-path --color=never --hidden --exclude .git")))
-  (consult-customize
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file
-   consult--source-recent-file consult--source-project-recent-file consult--source-bookmark
-   :preview-key "C-SPC"))
+    (setq consult-fd-args (concat fd-name " --full-path --absolute-path --color=never --hidden --exclude .git"))))
 
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package consult-todo
+  :config
+  (defconst consult-todo--narrow
+    '((?t . "TODO")
+      (?f . "FIXME")
+      (?h . "HACK")
+      (?d . "DEPRECATED")
+      (?n . "NOTE"))))
+
+;;; Project detection
+
+(use-package project
+  :config
+  (setq project-list-file (locate-user-emacs-file "projects"))
+  (setq project-switch-commands 'project-find-file))
+
+;;; Formatting
+(use-package apheleia
+  :config
+  (apheleia-global-mode +1))
 
 ;;; Key bindings
 
@@ -192,10 +228,21 @@
     :prefix "SPC"
     :global-prefix "C-SPC")
   (leader-def
-   "s"  '(:ignore t :which-key "search")
-   "sg" '(consult-ripgrep :which-key "grep")
-   "sf" '(find-file :which-key "files")
-   "sb" '(consult-buffer :which-key "buffers")
-   "ss" '(consult-line :which-key "current file")
-   "g"  '(:ignore t :which-key "git")
-   "gg" '(magit-status :which-key "magit status")))
+    "s"  '(:ignore t :which-key "search")
+    "c"  '(:ignore t :which-key "code")
+    "g"  '(:ignore t :which-key "git")
+    "p"  '(:ignore t :which-key "project")
+    "b"  '(:ignore t :which-key "buffer")
+    "."  '(find-file :which-key "navigate files")
+    "bf" '(apheleia-format-buffer :which-key "format buffer")
+    "bt" '(consult-todo :which-key "search buffer todos")
+    "bl" '(consult-line :which-key "search buffer lines")
+    "cf" '(apheleia-format-buffer :which-key "format buffer")
+    "gg" '(magit-status :which-key "magit status")
+    "pp" '(project-switch-project :which-key "switch project")
+    "pb" '(consult-project-buffer :which-key "project buffers")
+    "sg" '(consult-ripgrep :which-key "grep")
+    "sf" '(project-find-file :which-key "project files")
+    "sb" '(consult-buffer :which-key "buffers")
+    "ss" '(consult-line :which-key "current file")
+    "st" '(consult-todo-project :which-key "project todos")))
