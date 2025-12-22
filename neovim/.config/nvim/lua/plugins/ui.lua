cda9cbc20085a97b5800dcb0fba54690b83d74a3
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-12-22 22:04:54 +0000

    remove annoying popups

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -18,7 +18,6 @@ vim.pack.add({
 
 require("noice").setup({
 	presets = {
-
 		command_palette = true,
 		long_message_to_split = true,
 		lsp_doc_border = true,
@@ -27,9 +26,10 @@ require("noice").setup({
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true,
 		},
 		progress = {
-			view = "notify",
+			enabled = false,
 		},
 	},
 	views = {
@@ -39,7 +39,7 @@ require("noice").setup({
 			},
 		},
 		notify = {
-			replace = true,
+			enabled = false,
 		},
 	},
 	routes = {
