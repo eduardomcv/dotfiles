c7b04ff7bc241026b391e8ec53269ba10f606852
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-01-03 08:31:46 +0000

    update brew script

--- scripts/lib/brew.sh
+++ scripts/lib/brew.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 check_brew() {
-	# Install homebrew if brew command does not exist
-	if ! command -v brew >/dev/null 2>&1; then
-		/bin/bash -c \
-			"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-	fi
+    # Install homebrew if brew command does not exist
+    if ! command -v brew >/dev/null 2>&1; then
+        /bin/bash -c \
+            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    fi
 }
 
 install_brew() {
-	# Add tap for emacs
- 	brew tap d12frosted/emacs-plus
+    # Add tap for emacs
+    brew tap d12frosted/emacs-plus
 
-	brew install \
-		cmake \
-		stow \
-		ripgrep \
-		fd \
-		bat \
-		fzf \
-		zoxide \
-		eza \
-		lazygit \
-		tealdeer \
-		mise \
-		emacs-plus@30
+    brew install \
+        cmake \
+        libvterm \
+        stow \
+        ripgrep \
+        fd \
+        bat \
+        fzf \
+        zoxide \
+        eza \
+        lazygit \
+        tealdeer \
+        mise \
+        emacs-plus@30
 
-	brew install --cask \
-		font-iosevka \
-		font-iosevka-aile \
-		font-symbols-only-nerd-font \
-		ghostty \
-		thunderbird \
-		zen
+    brew install --cask \
+        font-iosevka \
+        font-iosevka-aile \
+        font-symbols-only-nerd-font \
+        ghostty \
+        thunderbird \
+        zen
 
-	# Compile latest neovim
-	brew install neovim --HEAD
+    # Compile latest neovim
+    brew install neovim --HEAD
 }
