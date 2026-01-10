2a647e886d4e905702d3f26e3459783d910d4761
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-01-10 02:11:47 +0000

    add automatic pyvenv trigger

--- emacs/.config/emacs/config/config-code.el
+++ emacs/.config/emacs/config/config-code.el
@@ -9,11 +9,23 @@
   :custom
   (pyvenv-default-virtual-env-name ".venv")
   :config
-  (pyvenv-mode 1))
+  (pyvenv-mode 1)
+
+  (defun custom/auto-activate-python-venv ()
+    "Activate .venv directory if it exists in the project root."
+    (let* ((project-root (locate-dominating-file buffer-file-name ".venv"))
+           (venv-path (and project-root (expand-file-name ".venv" project-root))))
+      (when (and venv-path (file-directory-p venv-path))
+        (pyvenv-activate venv-path))))
+
+  (add-hook 'python-mode-hook #'custom/auto-activate-python-venv)
+  (add-hook 'python-ts-mode-hook #'custom/auto-activate-python-venv))
 
 (use-package yasnippet
   :init
-  (yas-global-mode 1))
+  (yas-global-mode 1)
+  :config
+  (yas-reload-all))
 
 (use-package yasnippet-snippets
   :after yasnippet)
