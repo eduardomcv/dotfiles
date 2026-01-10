ef0b7fea5c964b09e8799644dd30a21ab422d220
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-01-10 22:50:36 +0000

    fix eldoc toggle

--- emacs/.config/emacs/config/config-eldoc.el
+++ emacs/.config/emacs/config/config-eldoc.el
@@ -7,10 +7,8 @@
 
 (use-package eldoc
   :ensure nil
-
   :custom
   (eldoc-echo-area-use-multiline-p t)
-
   :config
   (add-to-list 'display-buffer-alist
                '("^\\*eldoc"
@@ -18,9 +16,13 @@
                  (window-height . 0.20)
                  (dedicated . t)))
 
+  (defun custom/get-eldoc-window ()
+    (seq-find (lambda (w)
+                (string-prefix-p "*eldoc" (buffer-name (window-buffer w))))
+              (window-list)))
+
   (defun custom/close-eldoc-window-on-next-command ()
-    "Close eldoc window on the next command (unless switching windows)."
-    (let ((win (get-buffer-window "*eldoc*")))
+    (let ((win (custom/get-eldoc-window)))
       (if win
           (unless (or (eq (selected-window) win)
                       (memq this-command
@@ -31,20 +33,22 @@
                               evil-window-next
                               evil-window-prev
                               evil-window-mru
-                              handle-switch-frame)))
+                              handle-switch-frame
+                              mwheel-scroll
+                              scroll-up-command
+                              scroll-down-command)))
             (delete-window win))
         (remove-hook 'pre-command-hook #'custom/close-eldoc-window-on-next-command))))
 
   (defun custom/toggle-eldoc-tooltip ()
-    "Toggle eldoc window. If visible, will auto-close on the next cursor movement."
     (interactive)
-    (let ((win (get-buffer-window "*eldoc*")))
+    (let ((win (custom/get-eldoc-window)))
       (if win
           (progn
             (delete-window win)
-            (remove-hook 'pre-command-hook 'custom/close-eldoc-window-on-next-command))
+            (remove-hook 'pre-command-hook #'custom/close-eldoc-window-on-next-command))
         (eldoc-doc-buffer t)
-        (add-hook 'pre-command-hook 'custom/close-eldoc-window-on-next-command)))))
+        (add-hook 'pre-command-hook #'custom/close-eldoc-window-on-next-command)))))
 
 (provide 'config-eldoc)
 
