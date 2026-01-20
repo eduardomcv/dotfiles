258e2ecd0864c12f20fd6fa101689efeaf0c8c58
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-01-20 23:39:55 +0000

    add elisp formatter

--- emacs/.config/emacs/config/config-format.el
+++ emacs/.config/emacs/config/config-format.el
@@ -5,6 +5,10 @@
 
 ;;; Code:
 
+(use-package elisp-autofmt
+  :commands (elisp-autofmt-mode elisp-autofmt-buffer)
+  :hook (emacs-lisp-mode . elisp-autofmt-mode))
+
 (use-package apheleia
   :init
   (apheleia-global-mode 1)
@@ -16,6 +20,13 @@
         '(ruff-isort ruff))
   (setf (alist-get 'python-ts-mode apheleia-mode-alist)
         '(ruff-isort ruff))
+
+  (add-to-list 'apheleia-formatters
+               '(elisp-autofmt . ("elisp-autofmt")))
+
+  (add-to-list 'apheleia-mode-alist
+               '(emacs-lisp-mode . elisp-autofmt))
+
   :general
   (custom/leader-key
     "cf" '(apheleia-format-buffer :which-key "format buffer")
