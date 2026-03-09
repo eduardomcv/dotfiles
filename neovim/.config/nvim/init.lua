82113bcf82506230c5443a9fe7e067ff9d27bace
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-03-09 20:01:11 +0000

    add neovim

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+-------- GLOBAL VARIABLES --------
+-- Set global variables before
+-- loading anything else!
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+----------------------------------
+
+require("config")
+require("plugins")
