3757132a83f554bff5e5fcb7eaf07f28f6a6da34
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-04-10 18:50:02 +0100

    update treesitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,32 @@
+vim.pack.add({
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/windwp/nvim-ts-autotag",
+})
+
+local treesitter = require("nvim-treesitter")
+
+treesitter.install({
+	"bash",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"regex",
+	"vim",
+	"javascript",
+	"typescript",
+	"tsx",
+	"yaml",
+	"json",
+	"html",
+	"css",
+	"python",
+})
+
+require("nvim-ts-autotag").setup({})
+
+-- Autocmds
+
+-- Automatically run :TSUpdate when nvim-treesitter is updated via vim.pack.update()
 vim.api.nvim_create_autocmd("PackChanged", {
 	desc = "Handle nvim-treesitter updates",
 	group = vim.api.nvim_create_augroup("nvim-treesitter-pack-changed-update-handler", { clear = true }),
@@ -14,51 +43,3 @@ vim.api.nvim_create_autocmd("PackChanged", {
 		end
 	end,
 })
-
-vim.pack.add({
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
-	"https://github.com/windwp/nvim-ts-autotag",
-})
-
----@diagnostic disable-next-line: missing-fields
-require("nvim-treesitter.configs").setup({
-	auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true },
-	ensure_installed = {
-		"bash",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"regex",
-		"vim",
-		"javascript",
-		"typescript",
-		"tsx",
-		"yaml",
-		"json",
-		"html",
-		"css",
-		"python",
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<C-space>",
-			node_incremental = "<C-space>",
-			scope_incremental = false,
-			node_decremental = "<bs>",
-		},
-	},
-	textobjects = {
-		move = {
-			enable = true,
-			goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-			goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-			goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-			goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-		},
-	},
-})
-
-require("nvim-ts-autotag").setup({})
