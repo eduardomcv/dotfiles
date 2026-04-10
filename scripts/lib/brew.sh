077b03b8e061d96ac0ad4482d3ae49c23a2b3222
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-04-10 18:43:04 +0100

    update brew script

--- scripts/lib/brew.sh
+++ scripts/lib/brew.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 check_brew() {
-    # Install homebrew if brew command does not exist
-    if ! command -v brew >/dev/null 2>&1; then
-        /bin/bash -c \
-            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    fi
+	# Install homebrew if brew command does not exist
+	if ! command -v brew >/dev/null 2>&1; then
+		/bin/bash -c \
+			"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	fi
 }
 
 install_brew() {
-    # Add tap for emacs
-    brew tap d12frosted/emacs-plus
+	# Add tap for emacs
+	brew tap d12frosted/emacs-plus
 
-    brew install \
-        cmake \
-        libvterm \
-        stow \
-        ripgrep \
-        fd \
-        bat \
-        fzf \
-        zoxide \
-        eza \
-        tlrc \
-        lazygit \
-        mise \
-        emacs-plus
+	brew install \
+		cmake \
+		libvterm \
+		stow \
+		ripgrep \
+		fd \
+		bat \
+		fzf \
+		zoxide \
+		eza \
+		tlrc \
+		lazygit \
+		mise \
+		tree-sitter-cli \
+		neovim \
+		emacs-plus
 
-    brew install --cask \
-        font-iosevka \
-        font-symbols-only-nerd-font \
-        thunderbird \
-        zen
+	brew install --cask \
+		font-iosevka \
+		font-symbols-only-nerd-font \
+		thunderbird \
+		zen
 }
