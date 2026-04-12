d88c9cb2876051242f446b32e8f1718e34c2489c
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-04-12 22:44:34 +0100

    add back snacks picker and remove individual mini plugins

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,15 @@ vim.pack.add({ "https://github.com/folke/snacks.nvim" })
 
 require("snacks").setup({
 	bigfile = { enabled = true },
+	git = { enabled = true },
+	gitbrowse = { enabled = true },
+	indent = { enabled = true },
+	input = { enabled = true },
+	notifier = { enabled = true },
+	quickfile = { enabled = true },
+	scroll = { enabled = true },
+	rename = { enabled = true },
+	words = { enabled = true },
 	dashboard = {
 		enabled = true,
 		preset = {
@@ -39,15 +48,24 @@ require("snacks").setup({
 			{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 		},
 	},
-	git = { enabled = true },
-	gitbrowse = { enabled = true },
-	indent = { enabled = true },
-	input = { enabled = true },
-	notifier = { enabled = true },
-	quickfile = { enabled = true },
-	scroll = { enabled = true },
-	rename = { enabled = true },
-	words = { enabled = true },
+	picker = {
+		enabled = true,
+		layout = {
+			preset = "dropdown",
+			hidden = {
+				"preview",
+			},
+			layout = {
+				backdrop = false,
+				row = 4,
+				height = 0.7,
+			},
+		},
+		sources = {
+			files = { hidden = true },
+			grep = { hidden = true },
+		},
+	},
 })
 
 --- Autocmds
@@ -100,3 +118,126 @@ end, { desc = "Dismiss all notifications" })
 kset("n", "<leader>nh", function()
 	Snacks.notifier.show_history()
 end, { desc = "Notification history" })
+
+kset("n", "<C-p>", function()
+	Snacks.picker.smart()
+end, { desc = "Smart find files" })
+
+kset("n", "<leader>sc", function()
+	Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "Search config files" })
+
+kset("n", "<leader>sf", function()
+	Snacks.picker.files()
+end, { desc = "Search files" })
+
+kset("n", "<leader>sb", function()
+	Snacks.picker.buffers()
+end, { desc = "Search buffers" })
+
+kset("n", "<leader>sB", function()
+	Snacks.picker.grep_buffers()
+end, { desc = "Grep Open Buffers" })
+
+kset("n", "<leader>sg", function()
+	Snacks.picker.grep()
+end, { desc = "Grep" })
+
+kset("n", "<leader>s:", function()
+	Snacks.picker.command_history()
+end, { desc = "Search command history" })
+
+kset("n", "<leader>sC", function()
+	Snacks.picker.commands()
+end, { desc = "Search commands" })
+
+kset("n", "<leader>sn", function()
+	Snacks.picker.notifications()
+end, { desc = "Search notification history" })
+
+kset("n", "<leader>sp", function()
+	Snacks.picker.projects()
+end, { desc = "Search projects" })
+
+kset("n", "<leader>sr", function()
+	Snacks.picker.recent()
+end, { desc = "Search recent files" })
+
+kset({ "n", "x" }, "<leader>sw", function()
+	Snacks.picker.grep_word()
+end, { desc = "Visual selection or word" })
+
+kset("n", '<leader>s"', function()
+	Snacks.picker.registers()
+end, { desc = "Search registers" })
+
+kset("n", "<leader>s/", function()
+	Snacks.picker.search_history()
+end, { desc = "Search History" })
+
+kset("n", "<leader>sd", function()
+	Snacks.picker.diagnostics()
+end, { desc = "Search diagnostics" })
+
+kset("n", "<leader>sD", function()
+	Snacks.picker.diagnostics_buffer()
+end, { desc = "Search buffer diagnostics" })
+
+kset("n", "<leader>sh", function()
+	Snacks.picker.help()
+end, { desc = "Search help pages" })
+
+kset("n", "<leader>sk", function()
+	Snacks.picker.keymaps()
+end, { desc = "Search keymaps" })
+
+kset("n", "<leader>sm", function()
+	Snacks.picker.marks()
+end, { desc = "Search marks" })
+
+kset("n", "<leader>sM", function()
+	Snacks.picker.man()
+end, { desc = "Search man pages" })
+
+kset("n", "<leader>sl", function()
+	Snacks.picker.loclist()
+end, { desc = "Search location list" })
+
+kset("n", "<leader>sq", function()
+	Snacks.picker.qflist()
+end, { desc = "Search quickfix list" })
+
+kset("n", "<leader>su", function()
+	Snacks.picker.undo()
+end, { desc = "Search undo history" })
+
+kset("n", "<leader>st", function()
+	Snacks.picker.lsp_workspace_symbols()
+end, { desc = "Search LSP Workspace Symbols (tags)" })
+
+kset("n", "<leader>sT", function()
+	Snacks.picker.lsp_symbols()
+end, { desc = "Search LSP Symbols (tags)" })
+
+kset("n", "gd", function()
+	Snacks.picker.lsp_definitions()
+end, { desc = "Goto Definition" })
+
+kset("n", "gD", function()
+	Snacks.picker.lsp_declarations()
+end, { desc = "Goto Declaration" })
+
+kset("n", "gr", function()
+	Snacks.picker.lsp_references()
+end, {
+	nowait = true,
+	desc = "References",
+})
+
+kset("n", "gI", function()
+	Snacks.picker.lsp_implementations()
+end, { desc = "Goto Implementation" })
+
+kset("n", "gy", function()
+	Snacks.picker.lsp_type_definitions()
+end, { desc = "Goto T[y]pe Definition" })
