2c3488e2252c54b23bc6b05d5bdcc04aca561119
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-04-13 23:44:52 +0100

    fix script issues

--- scripts/lib/git.sh
+++ scripts/lib/git.sh
@@ -5,10 +5,10 @@ function set_user_git_info() {
 	local git_email=""
 
 	echo -n "(git config) Enter full name: "
-	IFS="\n" read -r git_username
+	IFS=$'\n' read -r git_username
 
 	echo -n "(git config) Enter email: "
-	IFS="\n" read -r git_email
+	IFS=$'\n' read -r git_email
 
 	git config --global user.name "$git_username"
 	git config --global user.email "$git_email"
