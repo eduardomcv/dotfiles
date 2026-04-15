6e826d13aab8627ef63a832bbf26ded3484f6ae2
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-04-15 01:09:51 +0100

    add backdrop

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -53,11 +53,12 @@ require("snacks").setup({
 	picker = {
 		enabled = true,
 		layout = {
+			preset = "dropdown",
 			hidden = {
 				"preview",
 			},
-			preset = "dropdown",
 			layout = {
+				backdrop = 70,
 				row = 4,
 				max_height = math.floor(vim.o.lines * 0.8),
 				width = math.floor(vim.o.columns * 0.5),
