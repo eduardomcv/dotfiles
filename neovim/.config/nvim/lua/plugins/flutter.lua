65b5b521fc9f61630edaf21f631c75d6cb0df095
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-06-23 12:39:12 +0100

    add keybinds to flutter tools

--- neovim/.config/nvim/lua/plugins/flutter.lua
+++ neovim/.config/nvim/lua/plugins/flutter.lua
@@ -14,4 +14,22 @@ require("flutter-tools").setup({
 	debugger = {
 		enabled = true,
 	},
+	lsp = {
+		on_attach = function(_, bufnr)
+			local function buf_set_keymap(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, desc = desc })
+			end
+
+			buf_set_keymap("n", "<leader>fr", ":FlutterRun<CR>", "Run Flutter app")
+			buf_set_keymap("n", "<leader>fq", ":FlutterQuit<CR>", "Quit Flutter app")
+			buf_set_keymap("n", "<leader>fr", ":FlutterReload<CR>", "Hot reload Flutter app")
+			buf_set_keymap("n", "<leader>fR", ":FlutterRestart<CR>", "Hot restart Flutter app")
+			buf_set_keymap("n", "<leader>fd", ":FlutterDevices<CR>", "List Flutter devices")
+			buf_set_keymap("n", "<leader>fe", ":FlutterEmulators<CR>", "List Flutter emulators")
+			buf_set_keymap("n", "<leader>fpg", ":FlutterPubGet<CR>", "Get Flutter dependencies")
+			buf_set_keymap("n", "<leader>fpu", ":FlutterPubUpgrade<CR>", "Upgrade Flutter dependencies")
+			buf_set_keymap("n", "<leader>fo", ":FlutterOutlineToggle<CR>", "Toggle Flutter outline")
+			buf_set_keymap("n", "<leader>fD", ":FlutterDevTools<CR>", "Open Flutter DevTools")
+		end,
+	},
 })
