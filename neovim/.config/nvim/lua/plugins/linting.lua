e561f38698d1f0b484e4567adca7216b3102218d
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-07-02 22:46:48 +0100

    improve bloc linter

--- neovim/.config/nvim/lua/plugins/linting.lua
+++ neovim/.config/nvim/lua/plugins/linting.lua
@@ -1,74 +1,48 @@
 vim.pack.add({ "https://github.com/mfussenegger/nvim-lint" })
 
-local bloc_lint_name = "bloc_lint"
+local linter_name = "bloc"
+local pattern = "(%w+)%[([%w_]+)%]:%s*(.-)\n%s*%-%->%s*.-:(%d+)"
+local severity_map = {
+	error = vim.diagnostic.severity.ERROR,
+	warning = vim.diagnostic.severity.WARN,
+	info = vim.diagnostic.severity.INFO,
+	hint = vim.diagnostic.severity.HINT,
+}
 
-local function bloc_parser(output)
+---@type lint.parse
+local function parse_bloc_output(output)
 	local diagnostics = {}
-	local current_diagostic = nil
 
-	local output_lines = vim.split(output, "\n")
-
-	for _, line in ipairs(output_lines) do
-		-- bloc lint output format: <severity>[<code>]: <message>
-		-- Example:
-		--   warning[avoid_flutter_imports]: Avoid importing Flutter within bloc instances.
-		local sev_str, code, msg = line:match("^(%w+)%[([%w_]+)%]:%s*(.*)")
-
-		if sev_str and code and msg then
-			local severity = vim.diagnostic.severity.INFO
-
-			if sev_str == "warning" then
-				severity = vim.diagnostic.severity.WARN
-			end
-
-			if sev_str == "error" then
-				severity = vim.diagnostic.severity.ERROR
-			end
-
-			current_diagostic = {
-				source = bloc_lint_name,
-				code = code,
-				message = msg,
-				severity = severity,
-			}
-		end
-
-		if current_diagostic then
-			-- The line immediately below the lint problem contains the file name and line number,
-			-- in the format: ---> <file>:<line_number>.
-			-- Example:
-			--   ---> lib/bloc/counter/counter_bloc.dart:10
-			local file, lnum = line:match("%-%->%s*(.-):(%d+)")
-
-			if file and lnum then
-				current_diagostic.lnum = tonumber(lnum) - 1
-				current_diagostic.col = 0
-
-				table.insert(diagnostics, current_diagostic)
-				-- Reset current_diagnostic after adding it to the diagostics table.
-				current_diagostic = nil
-			end
-		end
+	for sev_str, code, msg, lnum in output:gmatch(pattern) do
+		table.insert(diagnostics, {
+			source = linter_name,
+			code = code,
+			message = msg,
+			severity = severity_map[sev_str] or vim.diagnostic.severity.INFO,
+			lnum = tonumber(lnum) - 1,
+			col = 0,
+		})
 	end
 
 	return diagnostics
 end
 
-require("lint").linters[bloc_lint_name] = {
-	name = bloc_lint_name,
+require("lint").linters[linter_name] = {
+	name = linter_name,
 	cmd = "bloc",
 	args = { "lint" },
 	stdin = false,
 	append_fname = true,
 	stream = "stdout",
 	ignore_exitcode = true,
-	parser = bloc_parser,
+	parser = parse_bloc_output,
 }
 
 require("lint").linters_by_ft = {
 	python = { "ruff" },
 	bash = { "shellcheck" },
-	dart = { "bloc_lint" },
+	lua = { "luacheck" },
+	dart = { "bloc" },
 }
 
 --[[
