4efaa754614f04960ba05175f03cab911019f562
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-08-01 00:34:54 +0100

    update mason config

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -3,3 +3,28 @@ vim.pack.add({
 })
 
 require("mason").setup({})
+local registry = require("mason-registry")
+
+local packages_to_install = {
+	"lua-language-server",
+	"bash-language-server",
+	"json-lsp",
+	"yaml-language-server",
+	"vtsls",
+	"eslint-lsp",
+	"css-lsp",
+	"html-lsp",
+	"emmet-language-server",
+	"copilot-language-server",
+	"js-debug-adapter",
+	"debugpy",
+}
+
+registry.refresh(function()
+	for _, pkg_name in ipairs(packages_to_install) do
+		local ok, pkg = pcall(registry.get_package, pkg_name)
+		if ok and not pkg:is_installed() then
+			pkg:install()
+		end
+	end
+end)
