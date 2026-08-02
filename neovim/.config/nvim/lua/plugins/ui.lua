855b78767b7907ba05840d95fa601cda18e51410
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-08-02 01:22:04 +0100

    add navigation when not using kitty

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -13,8 +13,14 @@ vim.pack.add({
 local is_using_kitty = os.getenv("TERM") == "xterm-kitty" or os.getenv("KITTY_PID") ~= nil
 
 if is_using_kitty then
-	-- Integrate with kitty terminal
+	-- Integrate navigation with kitty terminal
 	vim.pack.add({ "https://github.com/knubie/vim-kitty-navigator" })
+else
+	-- Add C-h, C-j, C-k, C-l keymaps for window navigation
+	vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to the left window" })
+	vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to the down window" })
+	vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to the up window" })
+	vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to the right window" })
 end
 
 require("which-key").setup({
