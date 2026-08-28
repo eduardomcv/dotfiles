8902b3792a10096dc741510b1b40f10b06a03d6c
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-08-28 23:07:28 +0100

    add lua config to repo

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -53,40 +53,6 @@ vim.lsp.config("vtsls", {
 	},
 })
 
-vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {},
-	},
-	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if
-				path ~= vim.fn.stdpath("config")
-				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-			then
-				return
-			end
-		end
-
-		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-			runtime = {
-				version = "LuaJIT",
-				path = {
-					"lua/?.lua",
-					"lua/?/init.lua",
-				},
-			},
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					-- Make the server aware of Neovim runtime files
-					vim.env.VIMRUNTIME,
-				},
-			},
-		})
-	end,
-})
-
 local ruff_base_on_attach = vim.lsp.config.ruff.on_attach
 vim.lsp.config("ruff", {
 	cmd = function(dispatchers, config)
