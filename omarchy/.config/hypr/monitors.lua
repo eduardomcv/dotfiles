f4cffa72071694be7c571f8b612ea54c49bbb073
Eduardo Vicente <eduardomcvicente@gmail.com>
2026-08-29 21:28:16 +0100

    update monitors

--- omarchy/.config/hypr/monitors.lua
+++ omarchy/.config/hypr/monitors.lua
@@ -6,14 +6,24 @@ local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
 
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale })
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = omarchy_monitor_scale,
+})
 
-hl.monitor({ output = "DP-2", mode = "3440x1440@144", position = "0x0", scale = omarchy_monitor_scale })
+hl.monitor({
+	output = "DP-2",
+	mode = "3440x1440@144",
+	position = "0x0",
+	scale = omarchy_monitor_scale,
+})
 
 hl.monitor({
 	output = "DP-1",
 	mode = "2560x1440@144",
-	position = "auto",
+	position = "3440x0",
 	scale = omarchy_monitor_scale,
 	disabled = true,
 })
